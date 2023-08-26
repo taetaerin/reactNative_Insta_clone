20393b281e90d7e8f45e4b5978f13071a4748745
taetaerin <taerin13@naver.com>
2023-08-26 23:52:10 +0900

    SearchContent UI

--- src/components/SearchContent.js
+++ src/components/SearchContent.js
@@ -36,41 +36,83 @@ const searchData = [
 
 
 const SearchContent = () => {
-    
-  return (
-    <View>
-      {searchData.map((data, index) => {
+    return (
+        <View>
+            {searchData.map((data, index) => {
                 return (
                     <View key={index}>
-                        {data.id === 0 ? (
-                            <View
-                                style={{
-                                    flexDirection: 'row',
-                                    flexWrap: 'wrap',
-                                    justifyContent: 'space-between',
-                                    width: '100%'
-                                }}>
+                        {data.id === 0 ?
+                            (<View style={{
+                                flexDirection: 'row',
+                                flexWrap: 'wrap',
+                                justifyContent: 'space-between',
+                                width: '100%'
+                            }}>
                                 {data.images.map((imageData, imgIndex) => {
-                                    return (
+                                    return(
                                         <TouchableOpacity
                                             key={imgIndex}
-                                            onPressIn={() => props.data(imageData)}
-                                            onPressOut={() => props.data(null)}
-                                            style={{ paddingBottom: 2, width: '33%' }}>
-                                            <Image
+                                            style={{paddingBottom:2, width: '33%'}}
+                                        >
+                                            <Image 
                                                 source={imageData}
-                                                style={{ width: '100%', height: 150 }}
+                                                style={{width: '100%', height: 150}}
                                             />
                                         </TouchableOpacity>
-                                    );
+                                    )
                                 })}
+
+                            </View>)
+                        : null}
+
+                        {data.id ===1 ? (
+                            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                                <View style={{flexDirection: 'row', flexWrap: 'wrap', width: '66.5%', justifyContent: 'space-between'}}>
+                                    {data.images.slice(0,4).map((imageData, imgIndex) => {
+                                        return (
+                                            <TouchableOpacity 
+                                                key={imgIndex} 
+                                                style={{paddingBottom: 2, width: '49.5%'}}
+                                            >
+                                                <Image source={imageData} style={{width: '100%', height: 150}} />
+                                            </TouchableOpacity>
+                                        )
+                                    })}
+
+                                </View>
+                                <TouchableOpacity style={{marginLeft: 3, width: '33%'}}>
+                                    <Image 
+                                        source={data.images[4]}
+                                        style={{width: '100%', height: 300}}
+                                    />
+                                </TouchableOpacity>
                             </View>
                         ) : null}
-            </View>
-        )
-      })}
-    </View>
-  )
+
+                        {data.id === 2 ? (
+                            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                                <TouchableOpacity style={{paddingRight:2, width: '66.5%'}}>
+                                    <Image 
+                                        source={data.images[2]} 
+                                        style={{width:'100%', height: 300}} 
+                                    />
+                                </TouchableOpacity>
+                                <View style={{flexDirection: 'row', flexWrap: 'wrap', width: '33%', justifyContent: 'space-between'}}>
+                                    {data.images.slice(0,2).map((imageData, imgIndex) => {
+                                        return(
+                                            <TouchableOpacity key={imgIndex} style={{paddingBottom: 2, width: '100%'}}>
+                                                <Image source={imageData} style={{width: '100%', height: 150}} />
+                                            </TouchableOpacity>
+                                        )
+                                    })}
+                                </View>
+                            </View>
+                        ) : null}
+                    </View>
+                )
+            })}
+        </View>
+    )
 }
 
 export default SearchContent;
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -2,13 +2,14 @@ import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import SearchInput from '../components/SearchInput'
+import SearchContent from '../components/SearchContent'
 
 const Search = () => {
   return (
     <SafeAreaView style={{width: '100%', backgroundColor: 'white', position: 'relative'}}>
-      <ScrollView>
+      <ScrollView showsVerticalScrollIndicator={false}>
         <SearchInput />
-        
+        <SearchContent />
       </ScrollView>
     </SafeAreaView>
   )
