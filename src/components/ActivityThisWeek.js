444bb604ec4df9eb332c3459f4829a876c9dad07
taetaerin <taerin13@naver.com>
2023-08-29 23:52:52 +0900

    Activity 완성

--- src/components/ActivityThisWeek.js
+++ src/components/ActivityThisWeek.js
@@ -1,11 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { FriendsProfileData } from './Database';
 
 const ActivityThisWeek = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>ActivityThisWeek</Text>
-    </View>
+    <>
+      <Text style={{fontWeight: 'bold'}}>이번 주</Text>
+      <View style={{flexDirection: 'row', paddingVertical: 10}}>
+        {FriendsProfileData.slice(0,3).map((data, index) => {
+          return(
+            <TouchableOpacity
+              onPress={() => navigation.push('FriendProfile', {
+                name: data.name,
+                profile: data.profile,
+                follow: data.follow,
+                post: data.post,
+                followers: data.followers,
+                following: data.following
+              })}
+              key={index}
+            >
+              <Text>{data.name}{" "}</Text>
+            </TouchableOpacity>
+          )
+        })}
+        <Text>님이 팔로우 했습니다.</Text>
+      </View>
+    </>
   )
 }
 
--- src/screens/Activity.js
+++ src/screens/Activity.js
@@ -1,43 +1,43 @@
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
-import React from 'react'
-import ActivityThisWeek from '../components/ActivityThisWeek'
-import ActivityPrevious from '../components/ActivityPrevious'
-import ActivityRecommend from '../components/ActivityRecommend'
-import { FriendsProfileData } from '../components/Database'
-
+import React from 'react';
+import { Text, ScrollView, SafeAreaView } from 'react-native';
+import { FriendsProfileData } from '../components/Database';
+import ActivityThisWeek from '../components/ActivityThisWeek';
+import ActivityPrevious from '../components/ActivityPrevious';
+import ActivityRecommend from '../components/ActivityRecommend';
 
 const Activity = () => {
   return (
-    <SafeAreaView style={{width: '100%', backgroundColor: 'white'}}>
-      <Text 
+    <SafeAreaView style={{ width: '100%', backgroundColor: 'white' }}>
+      <Text
         style={{
           fontSize: 20,
           fontWeight: 'bold',
           borderBottomWidth: 0.5,
           borderBottomColor: '#DEDEDE',
           padding: 10,
-      }}>
+        }}>
         활동
       </Text>
+      <ScrollView style={{ margin: 10 }} showsVerticalScrollIndicator={false}>
 
-      <ScrollView style={{margin: 10}}>
         <ActivityThisWeek />
 
-        <Text style={{fontWeight: 'bold', paddingVertical: 10}}>이전 활동</Text>
-        {FriendsProfileData.slice(3,6).map((data, index) => {
+        <Text style={{ fontWeight: 'bold' }}>이전 활동</Text>
+        {FriendsProfileData.slice(3, 6).map((data, index) => (
           <ActivityPrevious data={data} key={index} />
-        })}
+        ))}
 
-        <Text style={{fontWeight: 'bold', paddingVertical: 10}}>회원님을 위한 추천</Text>
-        {FriendsProfileData.slice(6,12).map((data, index) => {
+
+        <Text style={{ fontWeight: 'bold', paddingVertical: 10 }}>
+          회원님을 위한 추천
+        </Text>
+        {FriendsProfileData.slice(6.12).map((data, index) => (
           <ActivityRecommend data={data} key={index} />
-        })}
-
-
+        ))}
 
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Activity
+export default Activity;
